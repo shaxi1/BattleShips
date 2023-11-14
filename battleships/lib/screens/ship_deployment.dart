b9303dba260c402f8af0c1e9eb28768b02412a6c
Marek Szulc <235991@edu.p.lodz.pl>
2023-11-14 16:50:15 +0100

    Add ship deployment screen

--- battleships/lib/screens/ship_deployment.dart
+++ battleships/lib/screens/ship_deployment.dart
@@ -1,21 +1,7 @@
-import 'package:battleships/theme.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-import '../structs/ship.dart';
-
-class Board {
-  late List<Ship?> grid;
-
-  Board(int size) {
-    grid = List<Ship?>.generate(size * size, (index) => null);
-  }
-
-  void placeShip(int index, Ship ship) {
-    // Implement logic to place ship on the board
-    // Check for valid placement, avoiding overlap with other ships
-    // You may want to handle edge cases based on the ship's rotation
-  }
-}
+import 'package:battleships/structs/ship.dart';
 
 class ShipPlacementScreen extends StatefulWidget {
   const ShipPlacementScreen({Key? key}) : super(key: key);
@@ -25,153 +11,122 @@ class ShipPlacementScreen extends StatefulWidget {
 }
 
 class _ShipPlacementScreenState extends State<ShipPlacementScreen> {
-  final int boardSize = 10; // Change this based on your requirement
-  final int maxShips = 10; // 1x4, 2x3, 3x2, 4x1
-  Board board = Board(10);
+  late List<Ship> board;
+
+  // Map ship sizes to corresponding image paths
+  Map<int, String> shipImagePaths = {
+    1: 'assets/ship_size1.png',
+    2: 'assets/ship_size2.png',
+    3: 'assets/ship_size3.png',
+    4: 'assets/ship_size4.png',
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    int boardLength = 10;
+    int boardSize = boardLength * boardLength;
+    board = List.generate(
+        boardSize, (index) => Ship(rotation: 'up', size: 0, shipPart: 'water'));
+    _initializeShips();
+  }
+
+  // Function to initialize ships randomly
+  void _initializeShips() {
+    // Initialize ships based on the given counts
+    Map<int, int> shipCounts = {1: 4, 2: 3, 3: 2, 4: 1};
+    for (int shipSize in shipCounts.keys) {
+      for (int i = 0; i < shipCounts[shipSize]!; i++) {
+        _initializeShip(shipSize, 10);
+      }
+    }
+  }
+
+  int _findShipHeadIndex(int shipSize, int boardLength, int boardSize) {
+    // choose random index for ship head position, based on board size, avoid edges (take into account the ship size, and the rotation up)
+    Random random = Random();
+    while (true) {
+      int headIndex = random.nextInt(boardSize - shipSize * boardLength);
+      print(headIndex);
+      // check if the ship can be placed at the chosen index
+      bool canPlace = true;
+      for (int i = 0; i < shipSize; i++) {
+        if (board[headIndex + i * boardLength].shipPart != 'water') {
+          canPlace = false;
+          break;
+        }
+      }
+      if (canPlace) {
+        return headIndex;
+      }
+    }
+  }
+
+  // All ships are initalized with rotation 'up'
+  void _initializeShip(int size, int boardLength) {
+    int headIndex =
+        _findShipHeadIndex(size, boardLength, boardLength * boardLength);
+
+    for (int i = 0; i < size; i++) {
+      Ship ship;
+      // head or tail of the ship
+      if (i == 0 || i == size - 1) {
+        ship = Ship(rotation: 'up', size: size, shipPart: 'edge');
+      } else {
+        ship = Ship(rotation: 'up', size: size, shipPart: 'middle');
+      }
+      board[headIndex + i * boardLength] = ship;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final isHorizontal =
-        MediaQuery.of(context).orientation == Orientation.landscape;
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
-
-    final boardHeight = screenHeight * 0.8;
-    final forVerticalshipsSectionHeight = screenHeight * 0.2;
-    final forHorizontalshipsSectionWidth = screenWidth * 0.2;
-
-    print(boardHeight);
-
     return Scaffold(
-      body: isHorizontal
-          ? _buildHorizontalLayout(boardHeight, forVerticalshipsSectionHeight)
-          : _buildVerticalLayout(boardHeight, forHorizontalshipsSectionWidth),
-    );
-  }
-
-  Widget _buildHorizontalLayout(
-      double boardHeight, double forHorizontalshipsSectionWidth) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 8,
-          child: Container(
-            height: boardHeight,
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: boardSize,
-              ),
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    // Handle ship placement here
-                    if (board.grid[index] == null) {
-                      Ship newShip =
-                          Ship(rotation: 'l', size: 1); // Customize as needed
-                      board.placeShip(index, newShip);
-                      setState(() {});
-                    }
-                  },
-                  child: Container(
-                    width: forHorizontalshipsSectionWidth,
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                    ),
-                    child: Center(
-                      child: board.grid[index]?.isHit == true
-                          ? Icon(Icons.close)
-                          : Text(
-                              board.grid[index]?.size.toString() ?? '',
-                              style: TextStyle(fontSize: 18.0),
-                            ),
-                    ),
-                  ),
-                );
-              },
-              itemCount: boardSize * boardSize,
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            height: boardHeight,
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            color: availShipsSectionBackgroundColor,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                // Generate a list of available ships (images or other widgets)
-                return ListTile(
-                  title: Text('Ship ${index + 1}'), // TODO: change to photo
-                  // Add ship images or other widgets here
-                );
-              },
-              itemCount: maxShips,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildVerticalLayout(double boardHeight, double shipsSectionHeight) {
-    return Column(
-      children: [
-        Container(
-          height: boardHeight,
-          child: GridView.builder(
+      appBar: AppBar(
+        title: Text('Battleship Board'),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.builder(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: boardSize,
+              crossAxisCount: orientation == Orientation.portrait ? 10 : 15,
             ),
             itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  // Handle ship placement here
-                  if (board.grid[index] == null) {
-                    Ship newShip =
-                        Ship(rotation: 'l', size: 1); // Customize as needed
-                    board.placeShip(index, newShip);
-                    setState(() {});
-                  }
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(),
-                  ),
-                  child: Center(
-                    child: board.grid[index]?.isHit == true
-                        ? Icon(Icons.close)
-                        : Text(
-                            board.grid[index]?.size.toString() ?? '',
-                            style: TextStyle(fontSize: 18.0),
-                          ),
-                  ),
-                ),
-              );
-            },
-            itemCount: boardSize * boardSize,
-          ),
-        ),
-        Container(
-          height: shipsSectionHeight,
-          color: Colors.grey[200],
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              // Generate a list of available ships (images or other widgets)
+              Ship ship = board[index];
+              String imagePath;
+              if (ship.shipPart == 'water') {
+                imagePath =
+                    'assets/water.png'; // Change this to your water image
+              } else {
+                imagePath = shipImagePaths[ship.size] ?? '';
+              }
+
+              // Rotate the image if ship's rotation is not 'up'
+              Widget imageWidget = ship.rotation != 'up'
+                  ? Transform.rotate(
+                      angle: ship.rotation == 'right' ? -pi / 2 : pi / 2,
+                      child: Image.asset(
+                        imagePath,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  : Image.asset(
+                      imagePath,
+                      fit: BoxFit.cover,
+                    );
+
+              // Add borders to simulate grid lines
               return Container(
-                width: 80.0, // Adjust as needed
-                margin: EdgeInsets.all(8.0),
-                child: ListTile(
-                  title: Text('Ship ${index + 1}'),
-                  // Add ship images or other widgets here
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
                 ),
+                child: imageWidget,
               );
             },
-            itemCount: maxShips,
-          ),
-        ),
-      ],
+            itemCount: board.length,
+          );
+        },
+      ),
     );
   }
 }
--- battleships/lib/structs/ship.dart
+++ battleships/lib/structs/ship.dart
@@ -2,6 +2,11 @@ class Ship {
   String rotation; // 'left', 'right', 'down', 'up'
   bool isHit;
   int size;
+  String shipPart; // 'edge' - head or tail, 'middle', 'water'
 
-  Ship({required this.rotation, this.isHit = false, required this.size});
+  Ship(
+      {required this.rotation,
+      this.isHit = false,
+      required this.size,
+      required this.shipPart});
 }
